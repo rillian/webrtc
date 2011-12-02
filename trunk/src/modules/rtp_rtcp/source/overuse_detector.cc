rillian/webrtc

commit 105c903ee6b65ae5a0bf37eed4148b6e66ec7806

Introduce a mockable RtpRtcpClock interface replacing ModuleRTPUtility time functions
A new RtpRtcpClock interface has been added to rtp_rtcp_defines.h
and provides time facilities used by an RTP/RTCP module. Also,
NTP constants have been made public in the
webrtc::ModuleRTPUtility namespace to make implementation of
external clocks easier.

An overloaded version of CreateRtpRtcp() accepts a clock argument. By
default, if no clock is provided, the module uses the system clock
(old ModuleRTPUtility implementation).

Throughout the RTP/RTCP module code, calls to TickTime and
ModuleRTPUtility time functions have been replaced with calls to time
methods on a clock object.

The following classes take a clock object in their constructor and
hold a _clock field (either directly, or inherited from a parent):

Bitrate
ModuleRtpRtcpImpl
RTCPReceiver
RTCPSender
RTPReceiver
RTPSender
RTPSenderAudio
RTPSenderVideo

Methods from other classes that do not derive any of those and
require a time take an additional nowMS parameter, that should be
the result of calling GetTimeInMS() on a clock object.
Review URL: http://webrtc-codereview.appspot.com/268017

git-svn-id: https://webrtc.googlecode.com/svn@1076 4adac7df-926f-26a2-2b94-8c16560cd09d

trunk/src/modules/rtp_rtcp/source/overuse_detector.cc
@@ -8,7 +8,6 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#include "tick_util.h"
 #include "trace.h"
 #include "overuse_detector.h"
 #include "remote_rate_control.h"
@@ -135,11 +134,13 @@ void OverUseDetector::Reset()
     }
 }
 
-bool OverUseDetector::Update(const WebRtcRTPHeader& rtpHeader, const WebRtc_UWord16 packetSize)
+bool OverUseDetector::Update(const WebRtcRTPHeader& rtpHeader,
+                             const WebRtc_UWord16 packetSize,
+                             const WebRtc_Word64 nowMS)
 {
 #ifdef MATLAB
     // Create plots
-    const WebRtc_Word64 startTimeMs = TickTime::MillisecondTimestamp();
+    const WebRtc_Word64 startTimeMs = nowMS;
     if (_plot1 == NULL)
     {
         _plot1 = eng.NewPlot(new MatlabPlot());
@@ -173,7 +174,6 @@ bool OverUseDetector::Update(const WebRtcRTPHeader& rtpHeader, const WebRtc_UWor
 
     bool wrapped = false;
     bool completeFrame = false;
-    const WebRtc_Word64 nowMs = TickTime::MillisecondTimestamp();
     if (_currentFrame._timestamp == -1)
     {
         _currentFrame._timestamp = rtpHeader.header.timestamp;
@@ -206,7 +206,7 @@ bool OverUseDetector::Update(const WebRtcRTPHeader& rtpHeader, const WebRtc_UWor
     }
     // Accumulate the frame size
     _currentFrame._size += packetSize;
-    _currentFrame._completeTimeMs = nowMs;
+    _currentFrame._completeTimeMs = nowMS;
     return completeFrame;
 }
 

trunk/src/modules/rtp_rtcp/source/remote_rate_control.cc
@@ -9,7 +9,6 @@
  */
 
 #include "remote_rate_control.h"
-#include "tick_util.h"
 #include "trace.h"
 #include <math.h>
 #include <string.h>
@@ -100,14 +99,17 @@ WebRtc_Word32 RemoteRateControl::SetConfiguredBitRates(WebRtc_UWord32 minBitRate
     return 0;
 }
 
-WebRtc_UWord32 RemoteRateControl::TargetBitRate(WebRtc_UWord32 RTT)
+WebRtc_UWord32 RemoteRateControl::TargetBitRate(WebRtc_UWord32 RTT,
+                                                WebRtc_Word64 nowMS)
 {
     _currentBitRate = ChangeBitRate(_currentBitRate, _currentInput._incomingBitRate,
-        _currentInput._noiseVar, RTT);
+        _currentInput._noiseVar, RTT, nowMS);
     return _currentBitRate;
 }
 
-RateControlRegion RemoteRateControl::Update(const RateControlInput& input, bool& firstOverUse)
+RateControlRegion RemoteRateControl::Update(const RateControlInput& input,
+                                            bool& firstOverUse,
+                                            WebRtc_Word64 nowMS)
 {
 #ifdef MATLAB
     // Create plots
@@ -141,10 +143,10 @@ RateControlRegion RemoteRateControl::Update(const RateControlInput& input, bool&
         {
             if (input._incomingBitRate > 0)
             {
-                _timeFirstIncomingEstimate = TickTime::MillisecondTimestamp();
+                _timeFirstIncomingEstimate = nowMS;
             }
         }
-        else if (TickTime::MillisecondTimestamp() - _timeFirstIncomingEstimate > 1000 &&
+        else if (nowMS - _timeFirstIncomingEstimate > 1000 &&
             input._incomingBitRate > 0)
         {
             _currentBitRate = input._incomingBitRate;
@@ -166,16 +168,18 @@ RateControlRegion RemoteRateControl::Update(const RateControlInput& input, bool&
 }
 
 WebRtc_UWord32 RemoteRateControl::ChangeBitRate(WebRtc_UWord32 currentBitRate,
-                                              WebRtc_UWord32 incomingBitRate, double noiseVar, WebRtc_UWord32 RTT)
+                                                WebRtc_UWord32 incomingBitRate,
+                                                double noiseVar,
+                                                WebRtc_UWord32 RTT,
+                                                WebRtc_Word64 nowMS)
 {
-    const WebRtc_Word64 now = TickTime::MillisecondTimestamp();
     if (!_updated)
     {
         return _currentBitRate;
     }
     _updated = false;
-    UpdateChangePeriod(now);
-    ChangeState(_currentInput, now);
+    UpdateChangePeriod(nowMS);
+    ChangeState(_currentInput, nowMS);
     // calculated here because it's used in multiple places
     const float incomingBitRateKbps = incomingBitRate / 1000.0f;
     // Calculate the max bit rate std dev given the normalized
@@ -214,7 +218,8 @@ WebRtc_UWord32 RemoteRateControl::ChangeBitRate(WebRtc_UWord32 currentBitRate,
 #endif
 #endif
             const WebRtc_UWord32 responseTime = static_cast<WebRtc_UWord32>(_avgChangePeriod + 0.5f) + RTT + 300;
-            double alpha = RateIncreaseFactor(now, _lastBitRateChange, responseTime, noiseVar);
+            double alpha = RateIncreaseFactor(nowMS, _lastBitRateChange,
+                                              responseTime, noiseVar);
 
             WEBRTC_TRACE(kTraceStream, kTraceRtpRtcp, -1,
                 "BWE: _avgChangePeriod = %f ms; RTT = %u ms", _avgChangePeriod, RTT);
@@ -245,7 +250,7 @@ WebRtc_UWord32 RemoteRateControl::ChangeBitRate(WebRtc_UWord32 currentBitRate,
             //TODO
 #endif
 #endif
-            _lastBitRateChange = now;
+            _lastBitRateChange = nowMS;
             break;
         }
     case kRcDecrease:
@@ -285,7 +290,7 @@ WebRtc_UWord32 RemoteRateControl::ChangeBitRate(WebRtc_UWord32 currentBitRate,
             }
             // Stay on hold until the pipes are cleared.
             ChangeState(kRcHold);
-            _lastBitRateChange = now;
+            _lastBitRateChange = nowMS;
             break;
         }
     }
@@ -295,7 +300,7 @@ WebRtc_UWord32 RemoteRateControl::ChangeBitRate(WebRtc_UWord32 currentBitRate,
         // Allow changing the bit rate if we are operating at very low rates
         // Don't change the bit rate if the send side is too far off
         currentBitRate = _currentBitRate;
-        _lastBitRateChange = now;
+        _lastBitRateChange = nowMS;
     }
 #ifdef MATLAB
     if (_avgMaxBitRate >= 0.0f)

trunk/src/modules/rtp_rtcp/interface/rtp_rtcp.h
@@ -22,13 +22,25 @@ class RtpRtcp : public Module
 {
 public:
     /*
-    *   create a RTP/RTCP module object
+    *   create a RTP/RTCP module object using the system clock
     *
     *   id      - unique identifier of this RTP/RTCP module object
     *   audio   - true for a audio version of the RTP/RTCP module object false will create a video version
     */
     static RtpRtcp* CreateRtpRtcp(const WebRtc_Word32 id,
                                   const bool audio);
+
+    /*
+    *   create a RTP/RTCP module object
+    *
+    *   id      - unique identifier of this RTP/RTCP module object
+    *   audio   - true for a audio version of the RTP/RTCP module object
+    *             false will create a video version
+    *   clock   - the clock to use to read time; must not be NULL
+    */
+    static RtpRtcp* CreateRtpRtcp(const WebRtc_Word32 id,
+                                  const bool audio,
+                                  RtpRtcpClock* clock);
 
     /*
     *   destroy a RTP/RTCP module object

trunk/src/modules/rtp_rtcp/interface/rtp_rtcp_defines.h
@@ -222,6 +222,22 @@ public:
 protected:
     virtual ~RtpVideoFeedback() {}
 };
+
+// A clock interface that allows reading of absolute and relative
+// timestamps in an RTP/RTCP module.
+class RtpRtcpClock {
+public:
+    // Return a timestamp in milliseconds relative to some arbitrary
+    // source; the source is fixed for this clock.
+    virtual WebRtc_UWord32 GetTimeInMS() = 0;
+
+    // Retrieve an NTP absolute timestamp.
+    virtual void CurrentNTP(WebRtc_UWord32& secs, WebRtc_UWord32& frac) = 0;
+
+protected:
+    virtual ~RtpRtcpClock() {}
+};
+
 } // namespace webrtc
 
 #endif // WEBRTC_MODULES_RTP_RTCP_INTERFACE_RTP_RTCP_DEFINES_H_

trunk/src/modules/rtp_rtcp/source/Bitrate.h
@@ -18,10 +18,12 @@
 #include <list>
 
 namespace webrtc {
+class RtpRtcpClock;
+
 class Bitrate
 {
 public:
-    Bitrate();
+    Bitrate(RtpRtcpClock* clock);
 
     // initialize members
     void Init();
@@ -40,6 +42,9 @@ public:
 
     // bitrate last second, updated now
     WebRtc_UWord32 BitrateNow() const;
+
+protected:
+    RtpRtcpClock&             _clock;
 
 private:
     WebRtc_UWord32            _packetRate;

trunk/src/modules/rtp_rtcp/source/bandwidth_management.cc
@@ -12,7 +12,6 @@
 #include "trace.h"
 #include "rtp_utility.h"
 #include "rtp_rtcp_config.h"
-#include "tick_util.h"
 
 #include <math.h>   // sqrt()
 
@@ -110,7 +109,8 @@ WebRtc_Word32 BandwidthManagement::UpdatePacketLoss(
     WebRtc_UWord32 sentBitrate,
     const WebRtc_UWord16 rtt,
     WebRtc_UWord8* loss,
-    WebRtc_UWord32* newBitrate)
+    WebRtc_UWord32* newBitrate,
+    WebRtc_Word64 nowMS)
 {
     CriticalSectionScoped cs(_critsect);
 
@@ -172,7 +172,7 @@ WebRtc_Word32 BandwidthManagement::UpdatePacketLoss(
     // Remember the sequence number until next time
     _lastPacketLossExtendedHighSeqNum = lastReceivedExtendedHighSeqNum;
 
-    WebRtc_UWord32 bitRate = ShapeSimple(*loss, rtt, sentBitrate);
+    WebRtc_UWord32 bitRate = ShapeSimple(*loss, rtt, sentBitrate, nowMS);
     if (bitRate == 0)
     {
         // no change
@@ -216,7 +216,8 @@ WebRtc_Word32 BandwidthManagement::CalcTFRCbps(WebRtc_Word16 avgPackSizeBytes,
 // protected
 WebRtc_UWord32 BandwidthManagement::ShapeSimple(WebRtc_Word32 packetLoss,
                                                 WebRtc_Word32 rtt,
-                                                WebRtc_UWord32 sentBitrate)
+                                                WebRtc_UWord32 sentBitrate,
+                                                WebRtc_Word64 nowMS)
 {
     WebRtc_UWord32 newBitRate = 0;
     bool reducing = false;
@@ -224,12 +225,12 @@ WebRtc_UWord32 BandwidthManagement::ShapeSimple(WebRtc_Word32 packetLoss,
     // Limit the rate increases to once a second.
     if (packetLoss <= 5)
     {
-        if ((TickTime::MillisecondTimestamp() - _timeLastIncrease) <
+        if ((nowMS - _timeLastIncrease) <
             kBWEUpdateIntervalMs)
         {
             return _bitRate;
         }
-        _timeLastIncrease = TickTime::MillisecondTimestamp();
+        _timeLastIncrease = nowMS;
     }
 
     if (packetLoss > 5 && packetLoss <= 26)

trunk/src/modules/rtp_rtcp/source/bandwidth_management.h
@@ -38,7 +38,8 @@ public:
         WebRtc_UWord32 sentBitrate,
         const WebRtc_UWord16 rtt,
         WebRtc_UWord8* loss,
-        WebRtc_UWord32* newBitrate);
+        WebRtc_UWord32* newBitrate,
+        WebRtc_Word64 nowMS);
 
     WebRtc_Word32 AvailableBandwidth(WebRtc_UWord16* bandwidthKbit) const;
 
@@ -51,7 +52,8 @@ public:
 protected:
     WebRtc_UWord32 ShapeSimple(WebRtc_Word32 packetLoss,
                                WebRtc_Word32 rtt,
-                               WebRtc_UWord32 sentBitrate);
+                               WebRtc_UWord32 sentBitrate,
+                               WebRtc_Word64 nowMS);
 
     WebRtc_Word32 CalcTFRCbps(WebRtc_Word16 avgPackSizeBytes,
                               WebRtc_Word32 rttMs,

trunk/src/modules/rtp_rtcp/source/bitrate.cc
@@ -10,12 +10,12 @@
 
 #include "Bitrate.h"
 #include "rtp_utility.h"
-#include "tick_util.h"
 
 #define BITRATE_AVERAGE_WINDOW 2000
 
 namespace webrtc {
-Bitrate::Bitrate() :
+Bitrate::Bitrate(RtpRtcpClock* clock) :
+    _clock(*clock),
     _packetRate(0),
     _bitrate(0),
     _bitrateNextIdx(0),
@@ -65,7 +65,7 @@ Bitrate::BitrateLast() const
 WebRtc_UWord32
 Bitrate::BitrateNow() const
 {
-    WebRtc_UWord32 now = ModuleRTPUtility::GetTimeInMS();
+    WebRtc_UWord32 now = _clock.GetTimeInMS();
     WebRtc_UWord32 diffMS = now -_timeLastRateUpdate;
 
     if(diffMS > 10000) // 10 sec
@@ -85,7 +85,7 @@ void
 Bitrate::Process()
 {
     // triggered by timer
-    WebRtc_UWord32 now = ModuleRTPUtility::GetTimeInMS();
+    WebRtc_UWord32 now = _clock.GetTimeInMS();
     WebRtc_UWord32 diffMS = now -_timeLastRateUpdate;
 
     if(diffMS > 100)

commit 11334b56df85a01b5ed53e022ad39ce8be312582

Fixes build error in rtp_rtc module introduced in r1076. Review URL: http://webrtc-codereview.appspot.com/301005
git-svn-id: https://webrtc.googlecode.com/svn@1081 4adac7df-926f-26a2-2b94-8c16560cd09d

trunk/src/modules/rtp_rtcp/source/overuse_detector.cc
@@ -8,6 +8,10 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#if _WIN32
+#include <windows.h>
+#endif
+
 #include "trace.h"
 #include "overuse_detector.h"
 #include "remote_rate_control.h"

trunk/src/modules/rtp_rtcp/source/remote_rate_control.cc
@@ -8,6 +8,10 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
+#if _WIN32
+#include <windows.h>
+#endif
+
 #include "remote_rate_control.h"
 #include "trace.h"
 #include <math.h>