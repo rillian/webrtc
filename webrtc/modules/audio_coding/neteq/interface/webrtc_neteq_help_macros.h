73594898627d80df474a5919b8754273deed5e57
Ralph Giles <giles@mozilla.com>
2012-11-15 12:48:26 -0800

    Add duration estimation for Opus.

--- webrtc/modules/audio_coding/neteq/interface/webrtc_neteq_help_macros.h
+++ webrtc/modules/audio_coding/neteq/interface/webrtc_neteq_help_macros.h
@@ -351,7 +351,7 @@
                     inst.funcGetMDinfo=NULL; \
                     inst.funcGetPitch=NULL; \
                     inst.funcUpdBWEst=NULL; \
-                    inst.funcDurationEst=NULL; \
+                    inst.funcDurationEst=(WebRtcNetEQ_FuncDurationEst)WebRtcOpus_DurationEst; \
                     inst.funcGetErrorCode=NULL;
 
 #define SET_OPUSSLAVE_FUNCTIONS(inst) \
@@ -363,7 +363,7 @@
                     inst.funcGetMDinfo=NULL; \
                     inst.funcGetPitch=NULL; \
                     inst.funcUpdBWEst=NULL; \
-                    inst.funcDurationEst=NULL; \
+                    inst.funcDurationEst=(WebRtcNetEQ_FuncDurationEst)WebRtcOpus_DurationEst; \
                     inst.funcGetErrorCode=NULL;
 
 #define SET_SPEEX_FUNCTIONS(inst) \
